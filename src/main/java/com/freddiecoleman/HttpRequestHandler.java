3bb8a431a0a6ea202154631d533ae198db0cd666
Freddie Coleman <f.coleman@hotmail.co.uk>
2015-06-03 20:00:48 +0100

    initial commit

---
+++ src/main/java/com/freddiecoleman/HttpRequestHandler.java
@@ -0,0 +1,16 @@
+package com.freddiecoleman;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.FullHttpRequest;
+
+public class HttpRequestHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
+
+	@Override
+	protected void channelRead0(ChannelHandlerContext ctx, FullHttpRequest request)
+			throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
